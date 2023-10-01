fd1211d8f25a53932a9a30b430c3bfe4955f12c0
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-10-01 12:50:14 +0300

    add infinite scroll behavior to store page

--- src/Hooks/useFetchGames.js
+++ src/Hooks/useFetchGames.js
@@ -3,22 +3,31 @@ import { useEffect, useState } from 'react';
 export default function useFetchGames(filter, getNew = false) {
   const [games, setGames] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    fetch(
-      `https://api.rawg.io/api/games?key=c93fe8e44e324c52879e1017192173b2&${filter}`
-    )
-      .then((response) => response.json())
-      .then((json) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.rawg.io/api/games?key=c93fe8e44e324c52879e1017192173b2&${filter}`
+        );
+        const data = await response.json();
         if (getNew) {
-          setGames(json.results);
+          setGames(data.results);
         } else {
           games
-            ? setGames([...games, ...json.results])
-            : setGames(json.results);
+            ? setGames([...games, ...data.results])
+            : setGames(data.results);
         }
+      } catch (error) {
+        setError(error);
+      } finally {
         setIsLoading(false);
-      });
+      }
+    };
+
+    setIsLoading(true);
+    fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filter, getNew]);
-  return [isLoading, games];
+  return [isLoading, games, error];
 }
--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { useState } from 'react';
 import useFetchGames from '../../Hooks/useFetchGames';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -5,24 +6,52 @@ import Card from 'react-bootstrap/Card';
 import styles from './Store.module.css';
 
 export default function Store() {
-  const [isLoading, games] = useFetchGames(`page=1`);
-  if (isLoading) return <p>Loading...</p>;
+  const [page, setPage] = useState(1);
+  const [isLoading, games, error] = useFetchGames(`page=${page}`);
+  const observerTarget = useRef(null);
+
+  useEffect(() => {
+    const observed = observerTarget.current;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((p) => p + 1);
+        }
+      },
+      { threshold: 1 }
+    );
+
+    if (observed) {
+      observer.observe(observed);
+    }
+
+    return () => {
+      if (observed) {
+        observer.unobserve(observed);
+      }
+    };
+  }, [observerTarget]);
+
+  if (error) return <p>{error}</p>;
   return (
     <div className={styles.store}>
       <div className={styles.games}>
-        {games.map((game) => (
-          <Card className={styles.card} key={game.id}>
-            <Card.Img
-              variant="top"
-              src={game.background_image}
-              className={styles.gameImage}
-            />
-            <Card.Body>
-              <Card.Title>{game.name}</Card.Title>
-            </Card.Body>
-          </Card>
-        ))}
+        {games &&
+          games.map((game) => (
+            <Card className={styles.card} key={game.id}>
+              <Card.Img
+                variant="top"
+                src={game.background_image}
+                className={styles.gameImage}
+              />
+              <Card.Body>
+                <Card.Title>{game.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          ))}
+        {isLoading && <div>loading...</div>}
       </div>
+      <div ref={observerTarget}></div>
     </div>
   );
 }
