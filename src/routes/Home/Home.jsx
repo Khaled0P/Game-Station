1dcb26a98529cf852721829d842c0feafd43b91f
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-10-11 22:12:28 +0300

    add ability to click homepage carousel items

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,8 +1,8 @@
-// import { useEffect, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import styles from './Home.module.css';
 import useFetchGames from '../../Hooks/useFetchGames';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   const [isLoading, popularGames, error] = useFetchGames('&metacritic=100');
@@ -30,33 +30,39 @@ export default function Home() {
       {popularGames && (
         <Carousel className={styles.carousel} fade="true">
           {popularGames.map((game) => (
-            <Carousel.Item key={game.id} className={styles.carouselItem}>
-              <div className={styles.image}>
-                <img src={game.background_image} alt={`${game.name}image`} />
-              </div>
-              <div className={styles.text}>
-                <h1>{game.name}</h1>
-                <div className={styles.screenShots}>
-                  {game.short_screenshots.slice(1, 5).map((screenshot) => (
-                    <img
-                      src={screenshot.image}
-                      alt="game screenshot"
-                      key={screenshot.id}
-                    />
-                  ))}
+            <Carousel.Item className={styles.carouselItem} key={game.id}>
+              <Link
+                to="/store/game"
+                state={{ id: game.id, screenshots: game.short_screenshots }}
+              >
+                <div className={styles.image}>
+                  <img src={game.background_image} alt={`${game.name}image`} />
                 </div>
-                <div>
-                  <p>
-                    <b>Genre: </b>
-                    {game.genres[0].name}, {game.genres[1].name}
-                  </p>
-                  <p>
-                    <b>Tags: </b>
-                    {game.tags[0].name}, {game.tags[1].name},{game.tags[2].name}
-                    , {game.tags[4].name}
-                  </p>
+                <div className={styles.text}>
+                  <h1>{game.name}</h1>
+                  <div className={styles.screenShots}>
+                    {game.short_screenshots.slice(1, 5).map((screenshot) => (
+                      <img
+                        src={screenshot.image}
+                        alt="game screenshot"
+                        key={screenshot.id}
+                        style={{ width: '190px', height: '105px' }}
+                      />
+                    ))}
+                  </div>
+                  <div>
+                    <p>
+                      <b>Genre: </b>
+                      {game.genres[0].name}, {game.genres[1].name}
+                    </p>
+                    <p>
+                      <b>Tags: </b>
+                      {game.tags[0].name}, {game.tags[1].name},
+                      {game.tags[2].name}, {game.tags[4].name}
+                    </p>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </Carousel.Item>
           ))}
         </Carousel>
