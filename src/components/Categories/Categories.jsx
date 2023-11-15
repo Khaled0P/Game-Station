07dccc9286dbc9b420f2ff22f175556973795ff5
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-11-15 20:25:15 +0200

    add categories responsive styles

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,10 +1,18 @@
 import { releases, top_games, platforms, genres } from './Data';
 import PropTypes from 'prop-types';
 import styles from './Categories.module.css';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { GamesContext, FetchContext } from '../Contexts/Contexts';
+import Filter from '../../assets/filter.svg?react';
+import Close from '../../assets/close.svg?react';
 
-function Category({ categoryContent, title, selected, setSelected }) {
+function Category({
+  categoryContent,
+  title,
+  selected,
+  setSelected,
+  setActive,
+}) {
   const { setPage, setGames } = useContext(FetchContext);
   const { fetchedGames, setFetchedGames } = useContext(GamesContext);
   function handleClick(item) {
@@ -14,6 +22,7 @@ function Category({ categoryContent, title, selected, setSelected }) {
     setGames(null);
     setPage(1);
     setFetchedGames(fetchFilter);
+    setActive((a) => !a);
   }
   return (
     <div className={styles.category}>
@@ -27,7 +36,7 @@ function Category({ categoryContent, title, selected, setSelected }) {
             key={item.name}
             onClick={() => handleClick(item)}
           >
-            <div className={styles.svgContainer}>{item.icon}</div>
+            <div className={styles.iconContainer}>{item.icon}</div>
             {item.name}
           </div>
         ))}
@@ -41,36 +50,74 @@ Category.propTypes = {
   title: PropTypes.string,
   selected: PropTypes.string,
   setSelected: PropTypes.func,
+  setActive: PropTypes.func,
 };
 
 export default function Categories() {
   const [selected, setSelected] = useState('All time top');
+  const [active, setActive] = useState(false);
+  const [preventTransition, setPreventTransition] = useState(false);
+
+  //clear responsive behavior on resize
+  useEffect(() => {
+    const removeActiveClass = () => {
+      if (window.innerWidth > 650) {
+        if (active) setActive(false);
+        setPreventTransition(true);
+      } else {
+        setPreventTransition(false);
+      }
+    };
+    window.addEventListener('resize', removeActiveClass);
+    return () => {
+      window.removeEventListener('resize', removeActiveClass);
+    };
+  });
+
   return (
-    <div className={styles.categories}>
-      <Category
-        categoryContent={top_games}
-        title="Top Games"
-        selected={selected}
-        setSelected={setSelected}
-      />
-      <Category
-        categoryContent={releases}
-        title="New Releases"
-        selected={selected}
-        setSelected={setSelected}
-      />
-      <Category
-        categoryContent={platforms}
-        title="Platforms"
-        selected={selected}
-        setSelected={setSelected}
-      />
-      <Category
-        categoryContent={genres}
-        title="Genres"
-        selected={selected}
-        setSelected={setSelected}
-      />
-    </div>
+    <>
+      <button
+        className={styles.menuButton}
+        onClick={() => {
+          setActive(!active);
+        }}
+      >
+        {active ? <Close /> : <Filter />}
+      </button>
+      <div
+        className={`${styles.categories}
+         ${active ? styles.activeMenu : ''}
+          ${preventTransition ? styles.preventTransition : ''}`}
+      >
+        <Category
+          categoryContent={top_games}
+          title="Top Games"
+          selected={selected}
+          setSelected={setSelected}
+          setActive={setActive}
+        />
+        <Category
+          categoryContent={releases}
+          title="New Releases"
+          selected={selected}
+          setSelected={setSelected}
+          setActive={setActive}
+        />
+        <Category
+          categoryContent={platforms}
+          title="Platforms"
+          selected={selected}
+          setSelected={setSelected}
+          setActive={setActive}
+        />
+        <Category
+          categoryContent={genres}
+          title="Genres"
+          selected={selected}
+          setSelected={setSelected}
+          setActive={setActive}
+        />
+      </div>
+    </>
   );
 }
